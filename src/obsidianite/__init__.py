945d950e5243a78cc18ac20e3b93875279883114
Sascha Mario Klein <sascha@neuroticfish.com>
2025-07-28 18:02:30 +0200

    fix: added __all__ to fix ruff check

--- src/obsidianite/__init__.py
+++ src/obsidianite/__init__.py
@@ -1,2 +1,4 @@
 from .note import Note
 from .notes import Notes
+
+__all__ = ["Note", "Notes"]
--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 from pathlib import Path
 from obsidianite import Notes, Note
 
